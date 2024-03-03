7ecab01df4f0ff7c3d185d7bef07826df54616a3
TosLICK <anatosliy@gmail.com>
2024-03-03 19:23:17 +0200

    First commit

---
+++ get_cats_info.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+def get_cats_info(path) -> list[dict]:
+    with open(path, 'r', encoding='utf-8') as file:
+        cats = file.readlines()
+        cats_info = []
+        for cat in cats:
+            cat_dict = {}
+            # cat_dict['id'], cat_dict['name'], cat_dict['age'] = cat.split(",")
+            id, name, age = cat.split(',')
+            cat_dict['id'], cat_dict['name'], cat_dict['age'] = id, name, int(age)
+            cats_info.append(cat_dict) # cat_dict перезаписується
+            # print(cat_dict)
+        return cats_info
+# get_cats_info("cats_file.txt")
+# print(get_cats_info("cats_file.txt"))
